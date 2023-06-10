df804859b7990e5bf01d57444d14470b03ebadc1
DESKTOP-NGUS3TH\stodr <stodrink@gmail.com>
2023-06-10 14:27:40 +0900

    VMの初期セットアップ用のシェルスクリプトを作成

---
+++ initial-setup.sh
@@ -0,0 +1,21 @@
+sudo bash -c '
+    apt update && apt install expect curl -y
+    
+    curl -fsSL https://get.docker.com -o /tmp/get-docker.sh && \
+    chmod u+x /tmp/get-docker.sh && \
+    (cd /tmp && ./get-docker.sh) && \
+    rm -f /tmp/get-docker.sh
+    
+    PASSWORD=rootroot && \
+    sed -i -e "/PasswordAuthentication/s/^/#/" -e "/PermitRootLogin/s/^/#/" /etc/ssh/sshd_config && \
+    echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config && \
+    echo "PermitRootLogin yes" >> /etc/ssh/sshd_config && \
+    expect -c "
+    spawn passwd
+    expect \"New password:\"
+    send \"${PASSWORD}\n\"
+    expect \"Retype new password:\"
+    send \"${PASSWORD}\n\"
+    expect " && \
+    service sshd restart
+'
