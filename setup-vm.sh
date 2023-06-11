4486e9a2394b8406313257e6afd202ae0fa0b290
stodr <78125061+s0meth1ng2dr1nk@users.noreply.github.com>
2023-06-11 13:51:41 +0900

    Update setup-vm.sh

--- setup-vm.sh
+++ setup-vm.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
+# export PASSWORD='' && apt update && apt install -y wget && wget https://raw.githubusercontent.com/s0meth1ng2dr1nk/util-tools/main/setup-vm.sh && chmod u+x ./setup-vm.sh && ./setup-vm.sh && rm -f ./setup-vm.sh
 set -eu
 sudo bash -c '
-    apt update && apt install expect curl git -y && \
+    PASSWORD='${PASSWORD}' && \
+    PORT=18022 && \
+    apt update && apt install -y expect curl git && \
     curl -fsSL https://get.docker.com -o /tmp/get-docker.sh && \
     chmod u+x /tmp/get-docker.sh && \
     (cd /tmp && ./get-docker.sh) && \
     rm -f /tmp/get-docker.sh && \
-    PASSWORD=rootroot && \
-    sed -i -e "/PasswordAuthentication/s/^/#/" -e "/PermitRootLogin/s/^/#/" /etc/ssh/sshd_config && \
+    sed -i -e "/PasswordAuthentication/s/^/#/" -e "/PermitRootLogin/s/^/#/" -e "/Port/s/^/#/" /etc/ssh/sshd_config && \
     echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config && \
     echo "PermitRootLogin yes" >> /etc/ssh/sshd_config && \
+    echo "Port ${PORT}" >> && \ 
     expect -c "
     spawn passwd
     expect \"New password:\"
