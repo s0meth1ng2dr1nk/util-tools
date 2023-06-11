8c9924a68d5c50eda14009190ab52bd8762da167
stodr <78125061+s0meth1ng2dr1nk@users.noreply.github.com>
2023-06-11 13:37:22 +0900

    Update and rename initial-setup.sh to setup-vm.sh

--- setup-vm.sh
+++ setup-vm.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 set -eu
 sudo bash -c '
-    apt update && apt install expect curl git -y
-    
+    apt update && apt install expect curl git -y && \
     curl -fsSL https://get.docker.com -o /tmp/get-docker.sh && \
     chmod u+x /tmp/get-docker.sh && \
     (cd /tmp && ./get-docker.sh) && \
-    rm -f /tmp/get-docker.sh
-    
+    rm -f /tmp/get-docker.sh && \
     PASSWORD=rootroot && \
     sed -i -e "/PasswordAuthentication/s/^/#/" -e "/PermitRootLogin/s/^/#/" /etc/ssh/sshd_config && \
     echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config && \
