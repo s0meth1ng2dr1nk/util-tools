f2adf9225389dde43c2876e7bca63ab2d7315c4b
DESKTOP-NGUS3TH\stodr <stodrink@gmail.com>
2023-09-25 23:30:03 +0900

    grep-keyを修正

--- grep-key.sh
+++ grep-key.sh
@@ -5,10 +5,10 @@ make_grep_key() {
   local L_STR="$3"
   F_LEN=${#F_STR}
   L_LEN=${#L_STR}
-  [ "${F_STR}" = '/' ] && F_STR=@SLASH
-  [ "${L_STR}" = '/' ] && L_STR=@SLASH
+  F_STR="$(echo ${F_STR} | sed -e 's#/#@SLASH#g')"
+  L_STR="$(echo ${L_STR} | sed -e 's#/#@SLASH#g')"
   echo "${S_VAL}" | sed -e 's/^ */ /g' -e 's/ *$/ /g' -e "s/ /${L_STR}|${F_STR}/g" -e 's#@SLASH#/#g' -e "s/^..\{${L_LEN}\}//" -e "s/..\{${F_LEN}\}$//g"
 }
 
 A=(A B C D E)
-make_grep_key "${A[*]}" "/" "bc"
+make_grep_key "${A[*]}" '/"/' "bc"
